a30b3ac3bf73fbb9d07a47912ef1e2026ca4f316
Rao Repalle <u292320@c02j621cdkq2.attlocal.net>
2015-08-21 17:09:24 -0700

    Initial Commit

---
+++ MusicSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicSearch
+//
+//  Created by Rao Repalle, Venkateswara R. on 8/21/15.
+//  Copyright (c) 2015 Pavani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MusicSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicSearch
+//
+//  Created by Rao Repalle, Venkateswara R. on 8/21/15.
+//  Copyright (c) 2015 Pavani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
